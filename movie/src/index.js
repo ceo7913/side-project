c809e1e8ceb3ae452102c1672c9a1a2ad99d1062
Lim JunWoo <ceo7913@naver.com>
2023-12-04 17:37:16 +0900

    docs: import index.js

--- movie/src/index.js
+++ movie/src/index.js
@@ -5,6 +5,15 @@ import {RouterProvider, createBrowserRouter} from 'react-router-dom'
 import { Main } from './pages/Main';
 import { NotFound } from './pages/NotFound';
 import { MainVideos } from './component/MainVideos';
+import { MovieList } from './component/MovieList';
+import { Provider } from 'react-redux';
+import {thunk} from 'redux-thunk'; // $ yarn add redux-thunk
+import { applyMiddleware, compose, createStore } from 'redux';
+import rootReducer from './store/index';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENTION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk))) 
+
 
 const router = createBrowserRouter([
   {
@@ -25,7 +34,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <GlobalStyle/>
-    <RouterProvider router={router}/>
     <MainVideos/>
+    <Provider store={store}>
+      <MovieList/>
+    </Provider>
+
+    <RouterProvider router={router}/>
   </>
 );
