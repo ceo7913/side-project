776e38e0fb21dd468cc755b2db5ddd6d8d005b0d
Lim JunWoo <ceo7913@naver.com>
2023-12-05 17:28:54 +0900

    docs: Create index file

--- movie/src/component/index.jsx
+++ movie/src/component/index.jsx
@@ -1,4 +1,9 @@
-import {default as MainVideos} from './MainVideos'
-import {default as MovieList} from './MovieList'
+import {MainVideos} from './MainVideos'
+import {MovieList} from './MovieList'
+import {Action} from './Action'
+import {Header} from './Header'
+import {Navigation} from './Navigation'
+import { OverView } from './OverView';
+import { Search } from './Search';
 
-export {MainVideos,MovieList}
+export {MainVideos,MovieList,Action,Header,Navigation,OverView,Search}
---
+++ movie/src/pages/index.jsx
@@ -0,0 +1,5 @@
+import {Main} from "./Main";
+import {MovieDetail} from "./MovieDetail";
+import {NotFound} from "./NotFound";
+
+export {Main, MovieDetail, NotFound}
