3f6f7ffc89f8dd1c40a6069130cc1c1b03ea6d6f
Lim JunWoo <ceo7913@naver.com>
2023-12-05 17:27:00 +0900

    chore: Change output file

--- movie/src/App.jsx
+++ movie/src/App.jsx
@@ -1,11 +1,24 @@
-// import React from 'react'
-// import './styled/App.css'
-// import { MainVideos, MovieList } from './component'
-// export const App = () => {
-//   return (
-//     <>
-//       <MainVideos/>
-//       <MovieList/>
-//     </>
-//   )
-// }
+import React from 'react';
+import GlobalStyle from './styled/GlobalStyled';
+import { Main } from './pages/Main';
+import {MainVideos, MovieList} from './component'
+import { Provider } from 'react-redux';
+import {thunk} from 'redux-thunk'; // $ yarn add redux-thunk
+import { applyMiddleware, compose, createStore } from 'redux';
+import rootReducer from './store/index';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENTION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk))) 
+
+export const App = () => {
+  return (
+    <>
+      <GlobalStyle/>
+      <MainVideos/>
+      <Main/>
+      <Provider store={store}>
+        <MovieList/>
+      </Provider>
+    </>
+  )
+}
--- movie/src/index.jsx
+++ movie/src/index.jsx
@@ -1,25 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import GlobalStyle from './styled/GlobalStyled';
 import {RouterProvider, createBrowserRouter} from 'react-router-dom'
-import { Main } from './pages/Main';
-import { NotFound } from './pages/NotFound';
-import { MainVideos } from './component/MainVideos';
-import { MovieList } from './component/MovieList';
-// import {MainVideos,MovieList} from './component';
-import { Provider } from 'react-redux';
-import {thunk} from 'redux-thunk'; // $ yarn add redux-thunk
-import { applyMiddleware, compose, createStore } from 'redux';
-import rootReducer from './store/index';
+import { App } from './App';
+import { MovieDetail, NotFound } from './pages';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENTION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk))) 
 
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Main/>,
+    element: <App/>,
     errorElement: <NotFound/>,
     children:[
       /*
@@ -27,19 +17,17 @@ const router = createBrowserRouter([
         중첩 라우터를 children 으로 연결하게 되면 내부에 있는 파일은 부모 요소의 링크를
         기준으로 잡힌다. 내부에 children 으로 작성하게 되면 중첩되는 urel 은 생략할 수 있다.
       */
-     // path: 
     ]
+  },
+  { 
+    // movie/movieId => 정해진 경로 / movie/:movieId => 유동적인 값 / => movieId 매개변수 같은 느낌으로 봐도 된다.
+    path: 'movie/:movieId',
+    element: <MovieDetail/>,
   }
 ])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-    <GlobalStyle/>
-    <MainVideos/>
-    <Provider store={store}>
-      <MovieList/>
-    </Provider>
-
+  <>  
     <RouterProvider router={router}/>
   </>
 );
