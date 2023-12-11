5f7ba42b3102bcba827da5b7ff50e24d8312bda0
Lim JunWoo <ceo7913@naver.com>
2023-12-11 15:00:35 +0900

    docs: uploadproduct component

--- shopping/src/pages/UpLoadProduct.jsx
+++ shopping/src/pages/UpLoadProduct.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 
 export const UpLoadProduct = () => {
+  const uploadSubmit = async(e) =>{
+    // async = 외부와 통신(가져오고 내보낼때)할때 사용
+    e.preventDefault(); // 기본 이벤트 제거
+
+  }
   return (
-    <div>UpLoadProduct</div>
+    <div className='container'>
+      <div className='imgUploadWrap'>
+
+      </div>
+      <form onSubmit={uploadSubmit}>
+        <input 
+          type="file" 
+          name='file'
+          accept='image/*'
+          onChange={imgUploadChange}
+        />
+      </form>
+    </div>
   )
 }
