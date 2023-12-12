c237326f29d3166261654dcdadc66276296af57b
Lim JunWoo <ceo7913@naver.com>
2023-12-12 17:49:01 +0900

    feat: Get state from DetailPageEvent to ProductDetail using useNavigate

--- shopping/src/component/DetailPageEvent.jsx
+++ shopping/src/component/DetailPageEvent.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
 import styled from 'styled-components'
+import { Link, useNavigate } from 'react-router-dom';
 
 export const DetailPageEvent = ({ product }) => {
+   // const colorItem = product.colors
+   /*
+      값을 변수에 담아서 사용했을 때와 아닐 때 차이
+      가독성 차이, 재사용성에도 장점
+
+      주의점: 변수에 담긴 값은 변수에 저장이 되버림, product.colors 의 값이 달라진다면,
+      colorItem 의 값은 변동이 없다. (object 의 특징)
+   */
+
+   /*
+       단순한 페이지의 이동이 목적이라면, Link 를 사용하면 되지만
+       현재의 경우 페이지를 이동하면서 데이터의 이동도 포함해야 하기 때문에 
+       useNavigate 를 사용한다. => ProductDetail.jsx
+   */
+   const navigate = useNavigate();
+   const detailNavigate = () => {
+      navigate(`/products/detail/${product.id}`, {
+         // 여기서의 state 는 네이밍을 바꾸면 error 출력
+         state: {
+            title: product.title,
+            id: product.id,
+            image: product.image,
+            price: product.price,
+            option: product.option,
+            category: product.category,
+            colors: product.colors,
+            description: product.description
+         }
+      })
+   }
    return (
-      <DetailItem>
+      <DetailItem onClick={detailNavigate}>
          <img src={product.image} alt={`${product.image}-img`} />
          <div className='textWrap'>
             <h3 className='itemTitle'>
@@ -18,10 +48,21 @@ export const DetailPageEvent = ({ product }) => {
                </p>
             </div>
             <div className='itemColors'>
-               {product.colors}
+               {/* {colorItem && colorItem.map((color, index) => (
+                  <div
+                     key={index}
+                     style={{ backgroundColor: color }} />     
+               ))} */}
+               {product.colors && product.colors.map((color, index) => (
+                  <div
+                     key={index}
+                     style={{ backgroundColor: color }} />
+               ))}
+               {/* 컬러 배열로 출력 */}
             </div>
          </div>
       </DetailItem>
+
    )
 }
 
@@ -46,6 +87,15 @@ const DetailItem = styled.div`
       .itemFlex{
          display: flex;
          justify-content: space-between;
+      }
+      .itemColors{
+         display: flex;
+         height: 20px;
+         gap:2px;
+         div{
+            width: 20px;
+            height: 20px;
+         }
       }
    }
 `
--- shopping/src/pages/ProductDetail.jsx
+++ shopping/src/pages/ProductDetail.jsx
@@ -1,7 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import styled from 'styled-components';
 
+// DetialPageEvent 에서 클릭한 정보값을 받아옴
 export const ProductDetail = () => {
-  return (
-    <div>ProductDetail</div>
-  )
+   // useLocation = useNavigate 에서 받아온 정보를 state 에 연결 하겠다는 구문
+   const state = useLocation().state
+   // console.log(state); // => {title: '텍스트 상품1', id: '94908969-2414-43d4-b8cb-1ed319ad0c00', image: 'http://res.cloudinary.com/dpuibr5sp/image/upload/v1702361179/f1idfn299l5cmfafmfr8.jpg', price: '216,600', option: 'free', …}
+   const { title, id, image, price, option, category, colors, description } = state;
+
+   console.log(option); // => option: 'S,M,L,XL' / 배열이 아니라 string 으로 담김
+   let setOpt = option.split(',').map((option) => option.trim()) // ',' 기준으로 trim 으로 잘라냄
+   // console.log(setOpt); // => ['S', 'M', 'L', 'XL']
+
+   const [selected, setSelected] = useState(setOpt && setOpt[0]); // => setOpt 배열이 있다면 setOpt 의 배열의 첫번째를 선택
+   const selectOpt = (e) => {
+      setSelected(e.target.value) // 선택한 setOpt 에 value 를 담음
+   }
+
+   return (
+      <div className='container'>
+         <DetailPage>
+            <div className='detailImg'>
+               <img src={image} alt={title} />
+            </div>
+            <div className='detailText'>
+               <h3>{title}</h3>
+               <p className='price'>가격 <span>{price}</span></p>
+               <p className='description'>{description}</p>
+
+               <div className='optSelect'>
+                  {/* 리액트에서는 label 의 for 대신에 htmlFor 로 변경하여 사용 */}
+                  <label className='labelText' htmlFor='optSelect'>옵션</label>
+                  <select id='optSelect' onChange={selectOpt} value={selected}>
+                     {setOpt && setOpt.map((option, index) => (
+                        <option key={index} value={option}>{option}</option>
+                     ))}
+                  </select>
+               </div>
+               <div style={{ display: 'flex', gap: '5px' }}>
+                  {colors && colors.map((color, idx) => (
+                     <div key={idx} style={{ backgroundColor: color, width: '20px', height: '20px' }} />
+                  ))}
+               </div>
+            </div>
+         </DetailPage>
+      </div>
+   )
 }
+
+const DetailPage = styled.div`
+   width: 100%;
+   display: flex;
+   gap: 40px;
+   .detailImg{
+      max-width: 400px;
+      img{
+         width: 100%;
+         display: block;
+      }
+   }
+   .detailText{
+      display: flex;
+      flex-direction: column;
+      gap: 20px;
+      width: 100%;
+      h3{
+         font-size: 24px;
+         width: 100%;
+         font-weight: normal;
+         border-bottom: solid 1px rgba(0,0,0,0.1);
+         padding-bottom: 12px;
+         padding-bottom: 20px;
+      }
+      .price{
+         display: flex;
+         align-items: center;
+         gap: 30px;
+      }
+   }
+`
