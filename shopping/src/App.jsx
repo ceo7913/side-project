359271a7bb246411f483b4aeb1417809c9e8e171
Lim JunWoo <ceo7913@naver.com>
2023-12-13 14:44:43 +0900

    build: react-query QueryClient

--- shopping/src/App.jsx
+++ shopping/src/App.jsx
@@ -2,23 +2,27 @@ import { Nav } from "./component/Nav";
 import { AuthContextProvider } from "./context/AuthContext";
 
 // 상위경로에서 하위경로 요소 연결 즉 routes 에 children 요소를 사용하기 위해 필요함
-import { Outlet, Route, Routes } from "react-router-dom"; 
+import { Outlet, Route, Routes } from "react-router-dom";
 
 // css
 import GlobalStyle from "./styled/GlobalStyled";
 import { AllProduct } from "./pages/AllProduct";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient()
 function App() {
   return (
     <>
-      <AuthContextProvider>
-        <GlobalStyle/>
-        <Nav/>
-        <Routes>
-            <Route path="/" element={<AllProduct/>}/>
-        </Routes>
-        <Outlet/> 
-      </AuthContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthContextProvider>
+          <GlobalStyle />
+          <Nav />
+          <Routes>
+            <Route path="/" element={<AllProduct />} />
+          </Routes>
+          <Outlet />
+        </AuthContextProvider>
+      </QueryClientProvider>
     </>
   );
 }
