daf605010bc516266010dc61742e38c01aeb54ad
Lim JunWoo <ceo7913@naver.com>
2023-12-13 14:48:13 +0900

    issue: Bad argument type

--- shopping/src/App.jsx
+++ shopping/src/App.jsx
@@ -13,6 +13,11 @@ const queryClient = new QueryClient()
 function App() {
   return (
     <>
+      {/* 
+      issue #1
+      Bad argument type. Starting with v5, only the "Object" form is allowed when calling query related functions. Please use the error stack to find the culprit call.
+      인수 유형이 잘못되었습니다. v5부터는 쿼리 관련 함수 호출 시 "Object" 형식만 허용됩니다. 오류 스택을 사용하여 범인 호출을 찾으십시오.
+    */}
       <QueryClientProvider client={queryClient}>
         <AuthContextProvider>
           <GlobalStyle />
